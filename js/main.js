42ec59bb77a4a26955d78cca077cf538c58d1054
jarmitage <jack.armitage@me.com>
2015-05-16 16:55:31 +0100

    Trying some formatting stuff. This will probably cause conflict :x

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 /* @flow */
-
-  window.onload = function(e) {
+window.onload = function(e) {
 
     var scene;
     var renderer;
@@ -10,7 +9,9 @@
     scene = new THREE.Scene();
 
     // renderer
-    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
     document.body.appendChild(renderer.domElement);
     renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -22,8 +23,22 @@
 
     // voronoi
     var voronoi = new Voronoi();
-    var bbox = {xl: 0, xr: window.innerWidth, yt: 0, yb: window.innerHeight};
-    var sites = [ {x: 100, y: 200}, {x: 50, y: 250}, {x: 50, y: 100} /* , ... */ ];
+    var bbox = {
+        xl: 0,
+        xr: window.innerWidth,
+        yt: 0,
+        yb: window.innerHeight
+    };
+    var sites = [{
+        x: 100,
+        y: 200
+    }, {
+        x: 50,
+        y: 250
+    }, {
+        x: 50,
+        y: 100
+    } /* , ... */ ];
     var diagram = voronoi.compute(sites, bbox);
 
     console.log(diagram);
@@ -31,16 +46,19 @@
     var geometry = new THREE.Geometry();
 
     for (var vertex in diagram.vertices) {
-      geometry.vertices.push(new THREE.Vector3(
-        diagram.vertices[vertex].x,
-        diagram.vertices[vertex].y,
-        0.0));
+        geometry.vertices.push(new THREE.Vector3(
+            diagram.vertices[vertex].x,
+            diagram.vertices[vertex].y,
+            0.0));
     }
 
-    var material = new THREE.LineBasicMaterial({color: 0xfff3ff, linewidth: 5});
+    var material = new THREE.LineBasicMaterial({
+        color: 0xfff3ff,
+        linewidth: 5
+    });
     var mesh = new THREE.Mesh(geometry, material);
 
     console.log(geometry.vertices);
 
     scene.add(mesh);
-  };
+};
