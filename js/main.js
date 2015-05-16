77967c63f770adf65df4ea86669f8ed420743027
jarmitage <jack.armitage@me.com>
2015-05-16 18:13:49 +0100

    Touch handlers working

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 /* @flow */
-var camera, scene, renderer, light, stats;
+var camera, scene, renderer, light, stats, container;
 
 init();
 animate();
 
 function init() {
 
+    scene = new THREE.Scene();
+    // renderer
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
     document.body.appendChild(renderer.domElement);
     renderer.setSize(window.innerWidth, window.innerHeight);
-
     // camera
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
     camera.position.set(0, 150, 500);
     light = new THREE.PointLight(0xffffff, 0.8);
     camera.add(light);
-
-    scene = new THREE.Scene();
-
     // voronoi
     var voronoi = new Voronoi();
     var bbox = {
@@ -36,47 +37,34 @@ function init() {
         y: 100
     } /* , ... */ ];
     var diagram = voronoi.compute(sites, bbox);
-
     console.log(diagram);
-
     var geometry = new THREE.Geometry();
-
     for (var vertex in diagram.vertices) {
         geometry.vertices.push(new THREE.Vector3(
             diagram.vertices[vertex].x,
             diagram.vertices[vertex].y,
             0.0));
     }
-
     var material = new THREE.LineBasicMaterial({
         color: 0xfff3ff,
         linewidth: 5
     });
     var mesh = new THREE.Mesh(geometry, material);
-
     console.log(geometry.vertices);
-
     scene.add(mesh);
-
-    // renderer
-    renderer = new THREE.WebGLRenderer({
-        antialias: true
-    });
 
     stats = new Stats();
     stats.domElement.style.position = 'absolute';
     stats.domElement.style.top = '0px';
-    container.appendChild(stats.domElement);
+    document.body.appendChild(stats.domElement);
 
     document.addEventListener('touchstart', onDocumentTouchStart, false);
     document.addEventListener('touchmove', onDocumentTouchMove, false);
-    document.addEventListener('touchend'), onDocumentTouchEnd, false);
+    document.addEventListener('touchend', onDocumentTouchEnd, false);
 }
 
 function onDocumentTouchStart(event) {
     if (event.touches.length === 1) {
-
-        event.preventDefault();
 
         console.log("Touch did start");
 
@@ -96,12 +84,10 @@ function onDocumentTouchMove(event) {
 }
 
 function onDocumentTouchEnd(event) {
-    if (event.touches.length == 1) {
-
-        event.preventDefault();
+    // if (event.touches.length == 1) {
 
         console.log("Touch did end");
-    }
+    // }
 }
 
 
