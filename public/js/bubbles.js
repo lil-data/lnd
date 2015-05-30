aa5e874ea3f699f64269ff887a24895d7c00f90d
Joe White <white.joe4@gmail.com>
2015-05-30 20:56:39 +0100

    correctly convert 2D mouse position to position in 3D

--- public/js/bubbles.js
+++ public/js/bubbles.js
@@ -12,6 +12,8 @@
     this.width = window.innerWidth;
     this.height = window.innerHeight;
 
+    this.needsUpdate = false;
+
     // init users
     this.users = {};
 
@@ -36,12 +38,10 @@
     });
 
     this.spheres = [];
-    this.mouseX = 0;
-    this.mouseY = 0;
 
     // init scene
     this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 100000);
-    this.camera.position.z = 3200;
+    this.camera.position.z = 2000;
     this.cameraCube = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 100000);
     this.scene = new THREE.Scene();
     this.sceneCube = new THREE.Scene();
@@ -91,24 +91,23 @@
     domElement.body.addEventListener('mousemove',
       function(event) {
         event.preventDefault();
-        that.user_position_updated(
-          event.clientX - (that.width / 2), (that.height / 2) - event.clientY);
-        this.mouseX = (event.clientX - (this.width / 2)) * 10;
-        this.mouseY = (event.clientY - (this.height / 2)) * 10;
+        that.user_position_updated(event.clientX, event.clientY);
       },
       false);
 
-    domElement.body.addEventListener('ontouchstart', function(event) {
-      event.preventDefault();
-      that.user_position_updated(
-        event.touches[0].pageX - (that.width / 2), (that.height / 2) - event.touches[0].pageY);
-    }, false);
+    domElement.body.addEventListener('ontouchstart',
+      function(event) {
+        event.preventDefault();
+        that.user_position_updated(event.touches[0].pageX, event.touches[0].pageY);
+      },
+      false);
 
-    domElement.body.addEventListener('touchmove', function(event) {
-      event.preventDefault();
-      that.user_position_updated(
-        event.touches[0].pageX - (that.width / 2), (that.height / 2) - event.touches[0].pageY);
-    }, false);
+    domElement.body.addEventListener('touchmove',
+      function(event) {
+        event.preventDefault();
+        that.user_position_updated(event.touches[0].pageX, event.touches[0].pageY);
+      },
+      false);
   };
 
   Bubbles.prototype.server_on_connection = function(users, userId) {
@@ -140,6 +139,7 @@
       img: user.img
     };
     this.create_sphere(userId, this.users[userId]);
+    this.needsUpdate = true;
   };
 
   Bubbles.prototype.server_user_removed = function(userId) {
@@ -153,16 +153,21 @@
     this.users[userId].x = user.x;
     this.users[userId].y = user.y;
     this.users[userId].img = user.img;
+    this.needsUpdate = true;
   };
 
   Bubbles.prototype.client_user_updated = function() {
     this.socket.emit('userDidUpdate', this.id, this.users[this.id]);
   };
 
-  Bubbles.prototype.user_position_updated = function(x, y) {
-    this.users[this.id].x = x;
-    this.users[this.id].y = y;
+  Bubbles.prototype.user_position_updated = function(canvasX, canvasY) {
+    var sceneX = (canvasX/this.width)*2 - 1;
+    var sceneY = 1 - 2*(canvasY/this.height);
+
+    this.users[this.id].x = sceneX;
+    this.users[this.id].y = sceneY;
     this.client_user_updated();
+    this.needsUpdate = true;
   };
 
   Bubbles.prototype.create_sphere = function(id, user) {
@@ -170,36 +175,36 @@
     mesh.name = id;
     mesh.position.x = user.x;
     mesh.position.y = user.y;
-    mesh.position.z = 1000;
+    mesh.position.z = 0;
     mesh.scale.x = mesh.scale.y = mesh.scale.z = 3;
     this.scene.add(mesh);
     this.spheres.push(mesh);
   };
 
   Bubbles.prototype.update_spheres = function(id, user) {
-    var current = {
-      x: this.scene.getObjectByName(id).position.x,
-      y: this.scene.getObjectByName(id).position.y
-    };
-    this.scene.getObjectByName(id).position.x = user.x*2;
-    this.scene.getObjectByName(id).position.y = user.y*2;
+    var mouseVec = new THREE.Vector3(user.x, user.y, 0.5);
+    mouseVec.unproject(this.camera);
+    mouseVec.sub(this.camera.position);
+    var dir = mouseVec.normalize();
+    var distance = -this.camera.position.z / dir.z;
+    var pos = this.camera.position.clone().add(dir.multiplyScalar(distance));
+
+    this.scene.getObjectByName(id).position.x = pos.x;
+    this.scene.getObjectByName(id).position.y = pos.y;
   };
 
   Bubbles.prototype.render = function() {
-    this.timer = 0.0001 * Date.now();
-    this.camera.position.x += (this.mouseX - this.camera.position.x) * .05;
-    this.camera.position.y += (-this.mouseY - this.camera.position.y) * .05;
-    this.camera.lookAt(this.scene.position);
-    this.cameraCube.rotation.copy(this.camera.rotation);
-    this.renderer.clear();
     this.renderer.render(this.sceneCube, this.cameraCube);
     this.renderer.render(this.scene, this.camera);
   };
 
   Bubbles.prototype.update = function() {
     // how to give this momentum?
-    for (var u in this.users) {
-      this.update_spheres(u, this.users[u]);
+    if (this.needsUpdate) {
+      for (var u in this.users) {
+        this.update_spheres(u, this.users[u]);
+      }
+      this.needsUpdate = false;
     }
   };
 
