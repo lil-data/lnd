3042d81a8e0df870e00062082ca4a26f67df8dbe
jarmitage <jack.armitage@me.com>
2015-06-02 17:02:13 +0100

    Changed port to 80 - run with sudo

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var express = require('express');
 var app = express();
 var users = {};
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 80));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
